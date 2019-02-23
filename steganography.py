94e0cf4b2358b6b4ab185e98a688db678ab3e859
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2019-02-24 02:17:40 +0330

    Fix steganography script

--- steganography.py
+++ steganography.py
@@ -8,43 +8,60 @@ MainHeader = '''
 '''
 print(MainHeader)
 
-image1 = cv2.imread('image.jpeg', 0)
-image2 = cv2.imread('image2.png', 0)
+class steganography:
+  def __init__(self):
+    # Assume Images have 8 bit to store colors
+    self.imageColorBits = 8
 
-def getMSBs(number, numberOfBits = 4):
-  a = number >> numberOfBits
-  b = a << numberOfBits
-  return b
+  def get_high_order_bits(self, number, numberOfBits = 4):
+    # Calculate number of bits to shift right
+    n = self.imageColorBits - numberOfBits
+    # Shift right to remove extra bits
+    temp = number >> n
+    # Return 8-bit Number with just <numberOfBits> hight value bits
+    return temp << n
+  
+  def set_high_order_as_low(self, number, numberOfBits = 4):
+    # Calculate number of bits to shift right
+    n = self.imageColorBits - numberOfBits
+    # Shift right to make high-order bits low-order
+    return number >> n
 
-def setMSBasLSB (number, numberOfBits = 4):
-  a = number >> numberOfBits
-  # b = a << numberOfBits
-  return a
+  def compress_image(self, image, setLowOrder = False):
+    """Compress image by removing low-order bits"""
+    h, w = image.shape
+    output = np.zeros([h,w], dtype=np.uint8)
 
-def compressImage(image, reverse = False):
-  h, w = image.shape
-  out = np.zeros([h,w], dtype=np.uint8)
+    for i in range(h):
+      for j in range(w):
+        temp = self.get_high_order_bits(image[i, j])
+        if setLowOrder: temp = self.set_high_order_as_low(temp)
+        output[i, j] = temp
+    return output
 
-  for i in range(h):
-    for j in range(w):
-      temp = getMSBs(image[i, j])
-      if reverse: temp = setMSBasLSB(temp)
-      out[i, j] = temp
-  return out
+  def encode_images(self, image1, image2):
+    if type(image1) == 'str': pass
+    if type(image1) == 'str': pass
+    
+    container = self.compress_image(image1)
+    containee = self.compress_image(image2, setLowOrder = True)
+    
+    return container | containee
+    
 
-def encodeImageInImage(image1, image2):
-  o1 = compressImage(image1)
-  o2 = compressImage(image2, True)
 
-  cv2.imshow("1"*100, o1)
+if __name__ == '__main__':
+  container = cv2.imread('images/container.png', 0)
+  containee = cv2.imread('images/containee.png', 0)
+
+  s = steganography()
+  output = s.encode_images(container, containee)
+
+  cv2.imshow("container", container)
   cv2.waitKey(0)
-  cv2.imshow("2"*100, o2)
+  cv2.imshow("containee", containee)
+  cv2.waitKey(0)
+  cv2.imshow("output", output)
   cv2.waitKey(0)
   
-  return o1 | o2
-
-
-out = encodeImageInImage(image1, image2)
-cv2.imshow("3"* 100, out)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+  cv2.destroyAllWindows()
