b83b4fe213223922d4bbdca5d6c079c42ba7dbe3
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2019-02-25 00:15:20 +0330

    Use numpy

--- steganography.py
+++ steganography.py
@@ -11,36 +11,31 @@ MainHeader = '''
 print(MainHeader)
 
 class Steganography:
-  
+
   def __init__(self, imageColorBits = 8):
     # Assume Images have 8 bit to store colors
     self.imageColorBits = imageColorBits
 
-  def get_high_order_bits(self, number, numberOfBits = 4):
-    # Calculate number of bits to shift right
-    n = self.imageColorBits - numberOfBits
-    # Shift right to remove extra bits
-    temp = number >> n
-    # Return 8-bit Number with just <numberOfBits> hight value bits
-    return temp << n
+  # def get_high_order_bits(self, number, numberOfBits = 4):
+  #   # Calculate number of bits to shift right
+  #   n = self.imageColorBits - numberOfBits
+  #   # Shift right to remove extra bits
+  #   temp = number >> n
+  #   # Return 8-bit Number with just <numberOfBits> hight value bits
+  #   return temp << n
   
-  def set_high_order_as_low(self, number, numberOfBits = 4):
-    # Calculate number of bits to shift right
-    n = self.imageColorBits - numberOfBits
-    # Shift right to make high-order bits low-order
-    return number >> n
+  # def set_high_order_as_low(self, number, numberOfBits = 4):
+  #   # Calculate number of bits to shift right
+  #   n = self.imageColorBits - numberOfBits
+  #   # Shift right to make high-order bits low-order
+  #   return number >> n
 
   def compress_image(self, image, setLowOrder = False, numberOfBits = 4):
     """Compress image by removing low-order bits"""
-    h, w = image.shape
-    output = np.zeros([h,w], dtype=np.uint8)
-
-    for i in range(h):
-      for j in range(w):
-        temp = self.get_high_order_bits(image[i, j], numberOfBits = numberOfBits)
-        if setLowOrder: temp = self.set_high_order_as_low(temp, numberOfBits = self.imageColorBits - numberOfBits)
-        output[i, j] = temp
-    return output
+    numberOfBits = self.imageColorBits - numberOfBits
+    imageShiftedRight = np.right_shift(image, numberOfBits)
+    if not setLowOrder: return np.left_shift(imageShiftedRight, numberOfBits)
+    else: return imageShiftedRight
 
   def encode_images(self, image1, image2):
     if type(image1) == 'str': pass
@@ -50,12 +45,15 @@ class Steganography:
     containee = self.compress_image(image2, setLowOrder = True)
     
     return container | containee
+
+  def funcname(self, parameter_list):
+    pass
     
 
 
 if __name__ == '__main__':
-  container = cv2.imread('images/container.png', 0)
-  containee = cv2.imread('images/containee.png', 0)
+  container = cv2.imread('images/container.png')
+  containee = cv2.imread('images/containee.png')
 
   s = Steganography()
   output = s.encode_images(container, containee)
