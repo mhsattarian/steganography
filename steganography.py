591061d15187f3b08d5a7ce2565ca46d6363a78e
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2019-02-25 03:02:07 +0330

    Add CLI features

--- steganography.py
+++ steganography.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import cv2
 import numpy as np
+import click
 
 class Steganography:
-
   def __init__(self, imageColorBits = 8):
     # Assume Images have 8 bit to store colors
     self.imageColorBits = imageColorBits
@@ -14,10 +14,10 @@ class Steganography:
     imageShiftedRight = np.right_shift(image, numberOfBits)
     if not setLowOrder: return np.left_shift(imageShiftedRight, numberOfBits)
     else: return imageShiftedRight
-
+  
   def encode_images(self, image1, image2):
-    if type(image1) == 'str': image1 = cv2.imread(image1)
-    if type(image2) == 'str': image2 = cv2.imread(image2)
+    if type(image1) == str: image1 = cv2.imread(image1)
+    if type(image2) == str: image2 = cv2.imread(image2)
     
     container = self.compress_image(image1)
     containee = self.compress_image(image2, setLowOrder = True)
@@ -25,12 +25,53 @@ class Steganography:
     return container | containee
 
   def decode_image(self, image, numberOfBits = 4):
+    if type(image) == str: image = cv2.imread(image)
+    
     numberOfBits = self.imageColorBits - numberOfBits
     codedImage = np.bitwise_and(image, pow(2, numberOfBits) - 1)
     return np.left_shift(codedImage, numberOfBits)
-    
+
+
 
 if __name__ == '__main__':
+
+  s = Steganography()
+
+  CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+  @click.group(context_settings=CONTEXT_SETTINGS)
+  def cli():
+      """Hide an image within another"""
+
+  @cli.command('encode', short_help='Encode an image within another')
+  @click.argument('container', type=click.Path(exists=True))
+  @click.argument('containee', type=click.Path(exists=True))
+  @click.option('-o', '--out', help="Specify output file", default="output.png")
+  @click.option('--show', help="Display output image", is_flag=True)
+  def encode(container, containee, out, show):
+    """Encode containee within container"""
+    output = s.encode_images(container, containee)
+    if show: 
+      cv2.imshow("output", output)
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
+    cv2.imwrite(out, output)
+    print("- output stored.")
+
+  @cli.command('decode', short_help='Extracts encoded image')
+  @click.argument('image', type=click.Path(exists=True))
+  @click.option('-o', '--out', help="Specify output file", default="extracted.png")
+  @click.option('--show', help="Display output image", is_flag=True)
+  def decode(image, out, show):
+    """Decode an image and store the extracted image"""
+    codedImage = s.decode_image(image)
+    if show: 
+      cv2.imshow("extracted", codedImage)
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
+    cv2.imwrite(out, codedImage)
+    print("- extracted image stored.")
+
+
   MainHeader = '''
   ┏━┓╺┳╸┏━╸┏━╸┏━┓┏┓╻┏━┓┏━╸┏━┓┏━┓┏━┓╻ ╻╻ ╻       Hide an Image within another
   ┗━┓ ┃ ┣╸ ┃╺┓┣━┫┃┗┫┃ ┃┃╺┓┣┳┛┣━┫┣━┛┣━┫┗┳┛                   by
@@ -39,18 +80,7 @@ if __name__ == '__main__':
   '''
   print(MainHeader)
 
-
-  container = cv2.imread('images/container.png')
-  containee = cv2.imread('images/containee.png')
-
-  s = Steganography()
-  output = s.encode_images(container, containee)
-  cv2.imwrite("images/output.png", output)
-  
-  codedImage = s.decode_image(output)
-  cv2.imwrite("images/coded.png", codedImage)
-
-  print("- output stored in Images directory.")
+  cli()
 
   # cv2.imshow("container", container)
   # cv2.waitKey(0)
@@ -59,4 +89,3 @@ if __name__ == '__main__':
   # cv2.imshow("output", output)
   # cv2.waitKey(0)
   
-  # cv2.destroyAllWindows()
